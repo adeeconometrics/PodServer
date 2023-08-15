a43f230b9283ab177eb76b551b963d7f633b3476
ddamiana <amianism25@gmail.com>
2023-08-15 22:49:51 +0800

    tcp updates

--- samples/src/tcpclient.cpp
+++ samples/src/tcpclient.cpp
@@ -5,7 +5,7 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-template <std::size_t BufferSize = 1024> struct TcpClient {
+struct TcpClient {
 
   TcpClient(const char *t_ip, const std::size_t t_port = 8080,
             const int t_backlog = 3)
@@ -14,29 +14,39 @@ template <std::size_t BufferSize = 1024> struct TcpClient {
     m_server_addr.sin_port = htons(t_port);
     inet_pton(AF_INET, t_ip, &m_server_addr.sin_addr);
 
-    connect(m_client_socket, (sockaddr *)&m_server_addr, sizeof(m_server_addr));
+    if (connect(m_client_socket,
+                reinterpret_cast<sockaddr *>(&m_server_addr) /*NOLINT*/,
+                sizeof(m_server_addr))) {
+      throw std::runtime_error("Connection failed");
+    }
   }
 
-  ~TcpClient() { close(m_client_socket); }
+  ~TcpClient() {
+    close(m_client_socket);
+  } // check is this one is closed when the server dies
 
   auto upload(const std::string &t_msg) const -> ssize_t {
     return send(m_client_socket, t_msg.data(), t_msg.size(), 0);
   }
 
   auto download() -> const std::string {
-    read(m_client_socket, m_buffer.data(), m_buffer.size());
-    return {m_buffer.data()};
+    const ssize_t bytesRead = read(m_client_socket, m_buffer.data(), m_buffer.size());
+    if (bytesRead < 0) {
+      throw std::runtime_error("Error reading from client socket");
+    }
+    return std::string(m_buffer.data(), static_cast<std::size_t>(bytesRead));
   }
 
 private:
-  std::array<char, BufferSize> m_buffer{};
+  static constexpr std::size_t buffer_size = 1024;
+  std::array<char, buffer_size> m_buffer{};
   sockaddr_in m_server_addr;
 
   int m_client_socket{};
 };
 
 auto main() -> int {
-  TcpClient<> client{"127.0.0.1"};
+  TcpClient client{"127.0.0.1"};
   client.upload("Hello, Server!");
   std::cout << "Client Mesage: " << client.download() << '\n';
 
--- samples/src/tcpserver.cpp
+++ samples/src/tcpserver.cpp
@@ -2,39 +2,102 @@
 #include <array>
 #include <iostream>
 #include <string>
+#include <string_view>
 #include <sys/socket.h>
 #include <unistd.h>
 
-template <std::size_t BufferSize = 1024> struct TcpServer {
+struct TcpSocket {
+public:
+  TcpSocket() : m_socket(-1) {}
+
+  explicit TcpSocket(int socket) : m_socket(socket) {}
+
+  TcpSocket(TcpSocket &&other) noexcept : m_socket(other.m_socket) {
+    other.m_socket = -1;
+  }
+
+  auto operator=(TcpSocket &&other) noexcept -> TcpSocket & {
+    if (this != &other) {
+      close();
+      m_socket = other.m_socket;
+      other.m_socket = -1;
+    }
+    return *this;
+  }
+
+  ~TcpSocket() { close(); }
+
+  operator int() const { return m_socket; }
+
+  auto close() -> void {
+    if (m_socket != -1) {
+      // ::close(m_socket);
+      m_socket = -1;
+    }
+  }
+
+private:
+  int m_socket;
+};
+
+struct TcpServer {
   TcpServer(const std::size_t t_port = 8080, const int t_backlog = 3)
       : m_server_socket(socket(AF_INET, SOCK_STREAM, 0)) {
+
+    if (m_server_socket < 0) {
+      throw std::runtime_error("Failed to create server socket");
+    }
+
     m_server_addr.sin_family = AF_INET;
     m_server_addr.sin_addr.s_addr = INADDR_ANY;
     m_server_addr.sin_port = htons(t_port);
 
-    bind(m_server_socket, (sockaddr *)&m_server_addr, sizeof(m_server_addr));
-    listen(m_server_socket, t_backlog);
-
-    sockaddr_in client_addr{};
-    socklen_t addrlen = sizeof(client_addr);
-    m_client_socket =
-        accept(m_server_socket,
-               reinterpret_cast<sockaddr *>(&client_addr) /*NOLINT*/, &addrlen);
+    setup_server(t_backlog);
+    accept_connection();
   }
 
   ~TcpServer() { close(m_server_socket); }
 
-  auto upload(const std::string &t_msg) const -> ssize_t {
+  auto upload(std::string t_msg) const -> ssize_t {
     return send(m_client_socket, t_msg.data(), t_msg.size(), 0);
   }
 
   auto download() -> const std::string {
-    read(m_client_socket, m_buffer.data(), m_buffer.size());
-    return {m_buffer.data()};
+    ssize_t bytes_read = read(m_client_socket, m_buffer.data(), m_buffer.size());
+    if(bytes_read < 0){
+      throw std::runtime_error("Error while reading data from client socket");
+    }
+    return std::string{m_buffer.data()};
   }
 
 private:
-  std::array<char, BufferSize> m_buffer{};
+  auto setup_server(const int t_backlog) -> void {
+    if (bind(m_server_socket,
+             reinterpret_cast<sockaddr *>(&m_server_addr) /*NOLINT*/,
+             sizeof(m_server_addr)) < 0) {
+      throw std::runtime_error("Bind failed");
+    }
+
+    if (listen(m_server_socket, t_backlog) < 0) {
+      throw std::runtime_error("Listen failed");
+    }
+  }
+
+  auto accept_connection() -> void {
+    sockaddr_in client_addr{};
+    socklen_t addrlen = sizeof(client_addr);
+    int client_socket =
+        accept(m_server_socket,
+               reinterpret_cast<sockaddr *>(&client_addr) /*NOLINT*/, &addrlen);
+    if (client_socket < 0) {
+      throw std::runtime_error("Accept failed");
+    }
+    m_client_socket = static_cast<int>(TcpSocket(client_socket));
+  }
+
+private:
+  static constexpr std::size_t buffer_size = 1024;
+  std::array<char, buffer_size> m_buffer{};
   sockaddr_in m_server_addr;
 
   int m_client_socket{};
@@ -42,7 +105,7 @@ private:
 };
 
 auto main() -> int {
-  TcpServer<> server{};
+  TcpServer server{};
   server.upload("Hello, Client!");
   std::cout << "Client Mesage: " << server.download() << '\n';
   return 0;
